06c21261d08114ea484d22f869a7de38b910ddbf
Giovanni Terlingen <gogio@live.nl>
2015-06-21 20:42:54 +0200

    Removed example and fixed log

--- src/nl/giovanniterlingen/whatsapp/message/BasicMessage.java
+++ src/nl/giovanniterlingen/whatsapp/message/BasicMessage.java
@@ -4,6 +4,8 @@ import nl.giovanniterlingen.whatsapp.ProtocolNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import android.util.Log;
+
 import java.util.Date;
 
 public class BasicMessage implements Message {
@@ -13,7 +15,6 @@ public class BasicMessage implements Message {
     private final String from;
     private final String groupId;
     private final Date date;
-    protected final Logger log = LoggerFactory.getLogger(BasicMessage.class);
 
     public BasicMessage(MessageType type, ProtocolNode node, String from, String groupId) {
         this.type = type;
@@ -21,14 +22,14 @@ public class BasicMessage implements Message {
         this.from = from;
         this.groupId = groupId;
         String t = node.getAttribute("t");
-        log.debug("t=" + t);
+        Log.d("DEBUG", "t=" + t);
         if (t != null) {
             long tl = Long.parseLong(t) * 1000;
             this.date = new Date(tl);
         } else {
             this.date = new Date();
         }
-        log.debug("date = " + date);
+        Log.d("DEBUG", "date = " + date);
     }
 
     public MessageType getType() {
