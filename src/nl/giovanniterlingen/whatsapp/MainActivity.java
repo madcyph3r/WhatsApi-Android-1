c6680c4185cd9f05d4865f75ad10cfa9d480a289
Giovanni Terlingen <gogio@live.nl>
2015-06-26 20:20:58 +0200

    Fixed: A problem when trying to request a sms
    Instead of null, a string value should have been added.

--- src/nl/giovanniterlingen/whatsapp/MainActivity.java
+++ src/nl/giovanniterlingen/whatsapp/MainActivity.java
@@ -8,8 +8,6 @@ import org.json.JSONObject;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,40 +15,43 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-	Button   mButton;
+	Button mButton;
 	EditText mEdit;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        
-        mButton = (Button)findViewById(R.id.verify_button);
-        mEdit   = (EditText)findViewById(R.id.number_text);
-        
-        mButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
-		WhatsApi wa = null;
-		try {
-			wa = new WhatsApi(MainActivity.this, mEdit.getText().toString(), null, null);
-			sendRequest(wa);
-		} catch (Exception e) {
-			Toast.makeText(MainActivity.this, "Caught exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-		}
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
+		mButton = (Button) findViewById(R.id.verify_button);
+		mEdit = (EditText) findViewById(R.id.number_text);
+
+		mButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				WhatsApi wa = null;
+				try {
+					wa = new WhatsApi(MainActivity.this, mEdit.getText()
+							.toString(), "mytestapplication", "mytestaccount");
+					sendRequest(wa);
+				} catch (Exception e) {
+					Toast.makeText(MainActivity.this,
+							"Caught exception: " + e.getMessage(),
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+
+		});
 	}
-                    
-    });
-}
 
 	private void sendRequest(WhatsApi wa) throws WhatsAppException,
 			JSONException, UnsupportedEncodingException {
 		JSONObject resp = wa.codeRequest("sms", null, null);
-		Toast.makeText(MainActivity.this, "Registration sent: " + resp.toString(2), Toast.LENGTH_SHORT).show();
+		Toast.makeText(MainActivity.this,
+				"Registration sent: " + resp.toString(2), Toast.LENGTH_SHORT)
+				.show();
 	}
 }
