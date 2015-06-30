469fb09f6fba8fca793d6abf65fb4e3ba8629f0d
Giovanni Terlingen <gogio@live.nl>
2015-06-30 10:38:46 +0200

    Registering works now, also added a better layout
    Just enter the verification code and your number to obtain your WhatsApp
    password.

--- src/nl/giovanniterlingen/whatsapp/MainActivity.java
+++ src/nl/giovanniterlingen/whatsapp/MainActivity.java
@@ -6,6 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -16,8 +17,13 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
 
 	Button mButton;
+	Button rButton;
 	EditText mEdit;
-
+	EditText mUser;
+	EditText mVerify;
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,14 +34,18 @@ public class MainActivity extends ActionBarActivity {
 		StrictMode.setThreadPolicy(policy);
 
 		mButton = (Button) findViewById(R.id.verify_button);
+		rButton = (Button) findViewById(R.id.register_button);
 		mEdit = (EditText) findViewById(R.id.number_text);
+		mUser = (EditText) findViewById(R.id.user_text);
+		mVerify = (EditText) findViewById(R.id.verficationcode_text);
 
 		mButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				WhatsApi wa = null;
 				try {
 					wa = new WhatsApi(MainActivity.this, mEdit.getText()
-							.toString(), "mytestapplication", "mytestaccount");
+							.toString(), "WhatsApi", mUser.getText()
+							.toString());
 					sendRequest(wa);
 				} catch (Exception e) {
 					Toast.makeText(MainActivity.this,
@@ -45,6 +55,35 @@ public class MainActivity extends ActionBarActivity {
 			}
 
 		});
+		
+		
+		rButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				
+			WhatsApi wa = null;
+				
+			
+			try {
+				wa = new WhatsApi(MainActivity.this, mEdit.getText()
+						.toString(), "WhatsApi", mUser.getText()
+						.toString());
+				sendRegister(wa);
+			} catch (Exception e) {
+				Toast.makeText(MainActivity.this,
+						"Caught exception: " + e.getMessage(),
+						Toast.LENGTH_SHORT).show();
+			}
+				
+			}
+			});
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	private void sendRequest(WhatsApi wa) throws WhatsAppException,
@@ -53,5 +92,30 @@ public class MainActivity extends ActionBarActivity {
 		Toast.makeText(MainActivity.this,
 				"Registration sent: " + resp.toString(2), Toast.LENGTH_SHORT)
 				.show();
+	}
+	
+	
+	
+	private void sendRegister(WhatsApi wa) throws JSONException, WhatsAppException {
+		String code = mVerify.getText()
+				.toString();
+		if(code == null || code.length() == 0) {
+			Toast.makeText(MainActivity.this,
+					"No verification code was entered", Toast.LENGTH_SHORT)
+					.show();
+			return;
+		}
+		JSONObject res = wa.codeRegister(code);
+		Toast.makeText(MainActivity.this,
+				"Registration was succesfull!", Toast.LENGTH_SHORT)
+				.show();
+		
+		ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+		clipboard.setText(res.toString(2));
+		
+		Toast.makeText(MainActivity.this,		
+				"Password has been copied to your clipboard", Toast.LENGTH_SHORT)
+				.show();
+				
 	}
 }
