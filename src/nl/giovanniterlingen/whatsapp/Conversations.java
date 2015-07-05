b113ef83ce0751402955ed5287ee33e6db41d99b
Giovanni Terlingen <gogio@live.nl>
2015-07-05 09:19:58 +0200

    Revert Alertdialog
    This alert isn't helpful. For example, when you don't have an internet
    connection, the same error shows up. Needs more work later.

--- src/nl/giovanniterlingen/whatsapp/Conversations.java
+++ src/nl/giovanniterlingen/whatsapp/Conversations.java
@@ -6,9 +6,6 @@ import java.security.NoSuchAlgorithmException;
 import org.apache.http.client.ClientProtocolException;
 
 import android.support.v7.app.ActionBarActivity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -33,35 +30,8 @@ public class Conversations extends ActionBarActivity {
 			e.printStackTrace();
 		} catch (ClientProtocolException e) {
 			Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
-		} catch (WhatsAppException e) { //At this moment, I always get authenticate trouble
-
-			new AlertDialog.Builder(this)
-					.setMessage(
-							"Error while logging in, please verify your number again")
-					.setPositiveButton("Verify",
-							new DialogInterface.OnClickListener() {
-
-								public void onClick(DialogInterface dialog,
-										int id) {
-									Intent intent = new Intent(
-											Conversations.this,
-											RegisterActivity.class);
-									startActivity(intent);
-									dialog.cancel();
-									finish();
-									return;
-								}
-							})
-					.setNegativeButton("Cancel",
-							new DialogInterface.OnClickListener() {
-
-								public void onClick(DialogInterface dialog,
-										int id) {
-									dialog.cancel();
-									return;
-								}
-							}).show();
-
+		} catch (WhatsAppException e) {
+			Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
 		} catch (IOException e) {
 			Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
 		}
