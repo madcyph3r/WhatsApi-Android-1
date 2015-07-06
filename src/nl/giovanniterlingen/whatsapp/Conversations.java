5fe6f8dd1b57d69c2eca99a3703360162940eed6
Giovanni Terlingen <gogio@live.nl>
2015-07-06 09:41:06 +0200

    Sending messages works now!

--- src/nl/giovanniterlingen/whatsapp/Conversations.java
+++ src/nl/giovanniterlingen/whatsapp/Conversations.java
@@ -3,6 +3,7 @@ package nl.giovanniterlingen.whatsapp;
 import android.support.v7.app.ActionBarActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
@@ -10,6 +11,11 @@ public class Conversations extends ActionBarActivity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
 		setContentView(R.layout.conversations);
 
 		SharedPreferences preferences = PreferenceManager
@@ -19,16 +25,21 @@ public class Conversations extends ActionBarActivity {
 			wa = new WhatsApi(Conversations.this, preferences.getString(
 					"number", ""), "WhatsApi", preferences.getString(
 					"username", ""));
-			wa.connect(); //Maybe I was forgotten this one
+			wa.connect(); // Maybe I was forgotten this one
 			wa.loginWithPassword(preferences.getString("pw", ""));
+
+			sendTextMessage(wa);
 
 		} catch (Exception e) {
 			Toast.makeText(this, "Caught exception: " + e.getMessage(),
 					Toast.LENGTH_SHORT).show();
 			e.printStackTrace();
-			if (wa != null) {
-				wa.disconnect();
-			}
 		}
+	}
+
+	private static void sendTextMessage(WhatsApi wa) throws WhatsAppException {
+		String to = "31612345678";
+		String message = "Test message from Giovanni's WhatsApp client";
+		wa.sendMessage(to, message);
 	}
 }
