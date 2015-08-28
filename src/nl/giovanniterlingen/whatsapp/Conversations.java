93cafca054d51e9680edb47d08bfadd120eb34b7
Alessio Cotardo <alessio.cotardo@gmail.com>
2015-08-28 21:35:42 +0200

    Added onActivityResult

--- src/nl/giovanniterlingen/whatsapp/Conversations.java
+++ src/nl/giovanniterlingen/whatsapp/Conversations.java
@@ -48,6 +48,7 @@ public class Conversations extends AppCompatActivity {
 	String nEdit;
 	EditText mEdit;
 	HorizontalScrollView attachmentPicker;
+	private static int RESULT_LOAD_IMAGE = 1;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -327,6 +328,27 @@ if (mEdit.getText().toString().isEmpty()) {
 			return date;
 		}
 	}
+
+@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            galleryImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+
 
 	protected void onResume() {
 		super.onResume();
